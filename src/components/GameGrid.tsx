1243b6b08f77c997b924468cc058f35b274afea8
Austin Stephens <astephens@kr.af.mil>
2023-11-24 16:29:41 -0700

    display generes

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -8,12 +8,12 @@ const GameGrid = () => {
 
     const { data, error, isLoading } = useGames()
 
-    const skeletonArray = [1,2,3,4,5, 6]
+    const skeletonArray = [1,2,3,4,5,6]
 
     return (
         <>
         {error && <Text>{error}</Text>}
-        <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding="10px" spacing={10}>
+        <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} padding="10px" spacing={3}>
             {isLoading && skeletonArray.map(item =>
                 <GameCardContainer key={item}>
                 <GameCardSkeleton/>
