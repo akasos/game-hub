352b6e9447dcd6de5441bb92412b61a877c8ee6d
Austin Stephens <astephens@kr.af.mil>
2023-11-27 09:21:40 -0700

    highlight selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,9 +5,10 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
     onSelectGenre: (genre: Genre) => void
+    selectedGenre: Genre | null
 }
 
-function GenreList({onSelectGenre}: Props) {
+function GenreList({onSelectGenre, selectedGenre}: Props) {
 
     const { data, isLoading, error } = useGenres()
 
@@ -21,7 +22,7 @@ function GenreList({onSelectGenre}: Props) {
                 <ListItem key={genre.id} paddingY="5px">
                     <HStack>
                         <Image boxSize="32px" borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
-                        <Button onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">{genre.name}</Button>
+                        <Button fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"} onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">{genre.name}</Button>
                     </HStack>
                 </ListItem>
             ))}
