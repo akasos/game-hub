929dcad12dd9ff2f95805f9d68077ec90ec2f5cd
Austin Stephens <astephens@kr.af.mil>
2023-12-02 10:10:58 -0700

    shipping static data

--- src/hooks/usePlatforms.tsx
+++ src/hooks/usePlatforms.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import useData from "./useData";
-
+import platforms from "../data/platforms"
 interface Platform {
     id: number,
     name: string,
     slug: string,
 }
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents")
+const usePlatforms = () => ({data: platforms, isLoading: false, error: false})
 
 export default usePlatforms;
