4c66c371f08aedfcd811948d9b237b1f2f611ffa
Austin Stephens <astephens@kr.af.mil>
2023-12-21 05:24:59 -0700

    implement infinite scrolling

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,8 @@
-import {useInfiniteQuery} from "@tanstack/react-query"
+import { useInfiniteQuery } from "@tanstack/react-query"
 
-import {FetchResponse} from "./useData"
-import {Platform} from "./usePlatforms"
-import {GameQuery} from "../App"
+import { FetchResponse } from "./useData"
+import { Platform } from "./usePlatforms"
+import { GameQuery } from "../App"
 import ApiClient from "../services/apiClient"
 
 const apiClient = new ApiClient<Game>("/games")
